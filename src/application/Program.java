09c05498fa210ba12d681f122210da1d9eec553f
Tiago de Freitas <tiagoabc@gmail.com>
2023-05-10 10:00:58 -0300

    First class: Position

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,12 @@
 package application;
 
+import application.boardgame.Position;
+
 public class Program {
 
 	public static void main(String[] args) {
-		System.out.println("Hello! World");
-
+		
+		Position pos = new Position(3, 5);
+		System.out.print("Teste: " + pos);
 	}
-
 }
